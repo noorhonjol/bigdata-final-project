62b255c68e16516dc4ec3128f228bccf3071cbce
noorhonjol <s12011175@stu.najah.edu>
2023-12-30 03:38:30 +0200

    write some comments and write date from file

--- python-producer-app/a.py
+++ python-producer-app/a.py
@@ -4,14 +4,15 @@ import json
 import random
 import time
 import zipfile
+from datetime import datetime
 from kafka import KafkaProducer
 
 
 # Define a class for Tweet
 class Tweet:
-    def __init__(self, id, user, text):
+    def __init__(self, id, user, text, date):
         self.id = int(id)
-        self.date = int(time.time() * 1000)
+        self.date = date
         self.user = user
         self.text = text
         self.retweets = int(random.random() * 10)
@@ -33,15 +34,25 @@ producer = KafkaProducer(
     value_serializer=lambda v: json.dumps(v).encode('utf-8')
 )
 
-
 # Function to process and send a line
 def process_and_send(row):
     try:
-        # Create a Tweet object
-        tweet = Tweet(id=row[1], user=row[4], text=row[5])
 
-        # Send the tweet
-        producer.send('t1', value=tweet.to_dict())
+        # read date from row and convert it date object
+        date_obj = datetime.strptime(row[2], "%a %b %d %H:%M:%S PDT %Y")
+
+        # convert object to string with provided format
+
+        iso_format_date = date_obj.strftime("%Y-%m-%dT%H:%M:%S%z")
+
+        # Create a Tweet object
+        tweet = Tweet(id=row[1], user=row[4], text=row[5], date=iso_format_date)
+
+        print(tweet.to_dict())
+
+        # Send the tweet to kafka
+        producer.send('tweets-topic', value=tweet.to_dict())
+
     except IndexError:
         print("Error: Line does not contain enough data.")
     except Exception as e:
@@ -52,8 +63,11 @@ def process_and_send(row):
 try:
     with zipfile.ZipFile('archive.zip', 'r') as z:
         with z.open('training.1600000.processed.noemoticon.csv') as f:
+
             text_file = io.TextIOWrapper(f, encoding='utf-8')
+
             reader = csv.reader(text_file)
+
             for i, row in enumerate(reader, 1):
                 process_and_send(row)
                 if i % 1000 == 0:
@@ -61,3 +75,8 @@ try:
 finally:
     producer.flush()
     producer.close()
+
+
+# %%
+
+# %%
--- spark-consumer-app/src/main/scala/Main.scala
+++ spark-consumer-app/src/main/scala/Main.scala
@@ -1,24 +1,24 @@
 import org.apache.log4j.BasicConfigurator
 import org.apache.log4j.varia.NullAppender
-import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.{DataFrame,  SparkSession}
 import org.apache.spark.sql.functions._
-import org.apache.spark.sql.types.{IntegerType, LongType, StringType, StructField, StructType}
+import org.apache.spark.sql.types.{ IntegerType, LongType, StringType, StructField, StructType}
 
 object Main {
 
   private object Configs {
     val mongodbUri: String = "mongodb://127.0.0.1:27017"
     val kafkaServer = "localhost:9092"
-    val topicSubscribed = "t1"
-    val topicPublished = "t2"
-    val databaseName = "zeft"
-    val collectionName = "zeft"
+    val topicSubscribed = "tweets-topic"
+    val topicPublished = "top-users-tweets-topic"
+    val databaseName = "big-data"
+    val collectionName = "tweets"
   }
 
   val schema: StructType = StructType(
     Array(
       StructField("id", LongType, nullable = true),
-      StructField("date", LongType, nullable = true),
+      StructField("date", StringType, nullable = true),
       StructField("user", StringType,nullable = true),
       StructField("text", StringType, nullable = true),
       StructField("retweets", IntegerType, nullable = true)
@@ -36,6 +36,7 @@ object Main {
 
     val query = dfFromStream.writeStream.foreachBatch { (batchDF: DataFrame, _: Long) =>
 
+      //write coming data from kafka to database
       writeToMongoDb(batchDF, Configs.databaseName, Configs.collectionName)
 
       statisticsOperationAndSendToKafka(spark)
@@ -45,6 +46,34 @@ object Main {
     }.start()
 
     query.awaitTermination()
+
+
+  }
+
+  /*
+    this function is main function in program
+    it do this :
+    1-read all database collection
+    2-make the required statistics on already read data (get most 20 users make that makes tweets)
+    3-send it to another topic in kafka to use it in backend later
+   */
+
+  private def statisticsOperationAndSendToKafka(spark: SparkSession): Unit = {
+    val dataBaseDf = readFromMongoDb(spark, Configs.databaseName, Configs.collectionName)
+    //early return when data base is not have data
+    if(dataBaseDf.count()==0){
+      return
+    }
+
+    val statsDf = aggregateData(dataBaseDf)
+
+    //this code convert whole dataFrame to one row of json format
+    val aggregatedDF = convertDataFrameForKafka(statsDf)
+
+    statsDf.show()
+
+    //send data statistics data to kafka
+    produceToKafka(aggregatedDF, Configs.topicPublished)
   }
 
   private def createSparkSession(): SparkSession = {
@@ -67,16 +96,18 @@ object Main {
       .option("subscribe", Configs.topicSubscribed)
       .load()
 
-    val dfWithTimestamp =df.selectExpr("CAST(value AS STRING) as json")
+    //convert data that come from stream from json format to df with structure of provided schema
+    val dfWithTimestamp = df.selectExpr("CAST(value AS STRING) as json")
       .select(from_json($"json", schema).as("data"))
       .select("data.*")
 
-    val dfWithDate = dfWithTimestamp.withColumn("date",to_timestamp(from_unixtime($"date"/ 1000)))
-
+    //convert date from string format to date object in dataframe
+    val dfWithDate = dfWithTimestamp.withColumn("date",date_format($"date", "yyyy-MM-dd'T'HH:mm:ss"))
 
     dfWithDate
   }
 
+  // write data to specific database and specific collection
   private def writeToMongoDb(dataFrame: DataFrame, databaseName: String, collectionName: String): Unit = {
     dataFrame.write
       .format("mongodb")
@@ -85,7 +116,7 @@ object Main {
       .option("collection", collectionName)
       .save()
   }
-
+  // read data from specific database and specific collection
   private def readFromMongoDb(spark: SparkSession, databaseName: String, collectionName: String): DataFrame = {
     spark.read.format("mongodb")
       .option("database", databaseName)
@@ -93,18 +124,7 @@ object Main {
       .load()
   }
 
-  private def statisticsOperationAndSendToKafka(spark: SparkSession): Unit = {
-    val dataBaseDf = readFromMongoDb(spark, Configs.databaseName, Configs.collectionName)
-
-    val statsDf = aggregateData(dataBaseDf)
-
-    val aggregatedDF = convertDataFrameForKafka(statsDf)
-
-    statsDf.show()
-
-    produceToKafka(aggregatedDF, Configs.topicPublished)
-  }
-
+  //send to specific topic a specific dataframe
   private def produceToKafka(dataFrame: DataFrame, topicName: String): Unit = {
     dataFrame.write
       .format("kafka")
@@ -112,13 +132,18 @@ object Main {
       .option("topic", topicName)
       .save()
   }
-
+  //convert whole dataframe to one JSON string
   private def convertDataFrameForKafka(dataFrame: DataFrame): DataFrame = {
+    /*
+      struct all columns in one column (each row contain struct {col1val,col2val,..})
+      then make collect all rows in one row contain list of struct then convert this row to json format
+    */
     dataFrame
       .agg(collect_list(struct(col("*"))).as("data"))
       .select(to_json(col("data")).as("value"))
   }
 
+  //get most 20 users that makes tweets
   private def aggregateData(dataFrame: DataFrame): DataFrame = {
     dataFrame.groupBy(col("user")).count().sort(col("count").desc).limit(20)
   }
